a1790653f37a7bb7dbef97c8885929743fc09845
Ben Catt <bcatt09@gmail.com>
2020-09-25 09:42:41 -0400

    Added Use Gatient check for Lapeer/Owosso

--- PlanCheckv2/PlanChecks/UseGatedChecks.cs
+++ PlanCheckv2/PlanChecks/UseGatedChecks.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using VMS.TPS.Common.Model.API;
 
 namespace VMS.TPS.PlanChecks
@@ -13,13 +14,13 @@ namespace VMS.TPS.PlanChecks
             DepartmentInfo.MachineNames.CEN_EX, 
             DepartmentInfo.MachineNames.CLA_EX, 
             DepartmentInfo.MachineNames.DET_IX, 
-            DepartmentInfo.MachineNames.FAR_IX, 
+            DepartmentInfo.MachineNames.FAR_IX,
+            DepartmentInfo.MachineNames.FLT_BackTB,
+            DepartmentInfo.MachineNames.FLT_FrontTB,
             DepartmentInfo.MachineNames.LAN_IX,
-            DepartmentInfo.MachineNames.LAP_IX,
             DepartmentInfo.MachineNames.MAC_IX,
             DepartmentInfo.MachineNames.NOR_EX,
-            DepartmentInfo.MachineNames.NOR_IX,
-            DepartmentInfo.MachineNames.OWO_IX
+            DepartmentInfo.MachineNames.NOR_IX
         };
 
         public UseGatedChecks(PlanSetup plan) : base(plan) { }
@@ -48,6 +49,32 @@ namespace VMS.TPS.PlanChecks
                     {
                         Result = "Warning";
                         ResultDetails = "Plan has a low number of fractions and looks to contain a 4D image.  Should \"Use Gated\" be checked?";
+                        ResultColor = "Gold";
+                    }
+                }
+                else
+                {
+                    Result = "Pass";
+                    ResultColor = "LimeGreen";
+                }
+            }
+            else if (MachineID == DepartmentInfo.MachineNames.LAP_IX ||
+                     MachineID == DepartmentInfo.MachineNames.OWO_IX)
+            {
+                bool DIBH = plan.StructureSet.Image.Series.Comment.ToLower().Contains("dibh");
+
+                if (DIBH)
+                {
+                    if (plan.UseGating)
+                    {
+                        Result = "";
+                        ResultDetails = "\"Use Gated\" is checked";
+                        ResultColor = "LimeGreen";
+                    }
+                    else
+                    {
+                        Result = "Warning";
+                        ResultDetails = "\"Use Gated\" is not checked and the plan appears to be DIBH";
                         ResultColor = "Gold";
                     }
                 }
